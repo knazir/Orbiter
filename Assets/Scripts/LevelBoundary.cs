1f46b8894e049165b1483c0d224caecfb24d1c60
knazir <knazir@stanford.edu>
2018-03-13 15:46:09 -0700

    Created demo and menu scenes. Added f fonts and music

---
+++ Assets/Scripts/LevelBoundary.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelBoundary : MonoBehaviour {
+	private void OnTriggerEnter2D(Collider2D other) {
+		if (other.CompareTag("Player")) SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
