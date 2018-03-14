46b90f0b2e262df8066e9b019866efd37015be97
Tariq Patanam <t.ahmed.p@gmail.com>
2018-03-14 09:17:40 -0700

    Some asteroids added to level

---
+++ Assets/Scripts/OrbitPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitPoint : MonoBehaviour {
+
+	[SerializeField] private float orbitSpeed = 20.0f;
+//	[SerializeField] private bool useLocalPos;
+	[SerializeField] private bool rotateClockwise = true;
+	private Vector3 orbitCenter;
+
+	void Start () {
+		orbitCenter = transform.parent.transform.position;
+	}
+	
+	void Update () {
+		Vector3 rotAxis = Vector3.forward;
+		if (rotateClockwise) rotAxis = Vector3.back;
+
+		transform.RotateAround (orbitCenter, rotAxis, orbitSpeed * Time.deltaTime);
+	}
+}
