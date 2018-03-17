051e45a46ef718336abf5a574844b5f85afdf2a8
Tariq Patanam <t.ahmed.p@gmail.com>
2018-03-17 02:16:39 -0700

    Cleaned up orbiting script

--- Assets/Scripts/OrbitNoRotate.cs
+++ Assets/Scripts/OrbitNoRotate.cs
@@ -13,21 +13,17 @@ public class OrbitNoRotate : MonoBehaviour {
 	[SerializeField] private float orbitSpeed = 0.01f;
 
 	private const string TRIGGER_NAME = "Planet Trigger";
-	private const float ANGLE_EPSILON = 10.0f;
 
+	private const float ANGLE_EPSILON = 10.0f;
 	private const float behindPlanetZPos = 1f;
 	private const float besidePlanetZPos = 0.0f;
 	private const float frontPlanetZPos = -1f;
-	private bool atPlanet = false;
-	private bool movingToEnd = true;
 
-//	private Renderer orbiterRenderer;
+	private bool atPlanet = false;
 
 	void Start() {
 		// Ignore collisions between orbitting planet and moon
 		Physics2D.IgnoreCollision(GetComponent<Collider2D>(), transform.parent.parent.gameObject.GetComponent<Collider2D>());
-
-//		orbiterRenderer = GetComponent<Renderer> ();
 	}
 
 	void Update() {
@@ -38,13 +34,10 @@ public class OrbitNoRotate : MonoBehaviour {
 		
 		if (shouldBeBehindPlanet) {
 			transform.localPosition = new Vector3 (nextPosXY.x, nextPosXY.y, behindPlanetZPos);
-//			orbiterRenderer.enabled = false;
 		} else if (atPlanet && !shouldBeBehindPlanet) {
 			transform.localPosition = new Vector3 (nextPosXY.x, nextPosXY.y, frontPlanetZPos);
-//			orbiterRenderer.enabled = true;
 		} else {
 			transform.localPosition = new Vector3 (nextPosXY.x, nextPosXY.y, besidePlanetZPos);
-//			orbiterRenderer.enabled = true;
 		}
 	}
 
@@ -57,7 +50,6 @@ public class OrbitNoRotate : MonoBehaviour {
 	}
 
 	void OnTriggerExit2D(Collider2D col) {
-//		GameObject ancestorObject = transform.parent.parent.gameObject;
 		if (col.gameObject.name == TRIGGER_NAME) {
 			// No longer behind
 			atPlanet = false;
@@ -66,9 +58,6 @@ public class OrbitNoRotate : MonoBehaviour {
 
 	bool isMovingToEnd(Vector2 prevPos, Vector2 nextPos, Vector2 start, Vector2 end) {
 		// Checks if two vectors are close to parallel
-//		print ("End to start normalized, " + (end - start).normalized);
-//		print ("Next to prev normalized, " + (nextPos - prevPos).normalized);
-//		Vector2 epsilon = new Vector2 (0.1f, 0.1f);
 
 		Vector3 movementVec = new Vector3((nextPos - prevPos).x, (nextPos - prevPos).y, 0);
 		Vector3 forwardVec = new Vector3((end - start).x, (end - start).y, 0);
@@ -77,23 +66,4 @@ public class OrbitNoRotate : MonoBehaviour {
 
 		return (angle < ANGLE_EPSILON);
 	}
-
-//	private float radius;
-//	private float angle = 0.0f;
-//
-////	[SerializeField] private bool 
-//
-//	// Use this for initialization
-//	void Start () {
-//		radius = transform.localPosition.x;
-//	}
-//	
-//	// Update is called once per frame
-//	void Update () {
-//		angle += RotateSpeed * Time.deltaTime;
-//
-//		Vector2 realOffset = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle)) * radius;
-//		Vector3	totalOffset = new Vector3 (realOffset.x, 0, realOffset.y);
-//		transform.position = pivot.position + totalOffset;
-//	}
 }
