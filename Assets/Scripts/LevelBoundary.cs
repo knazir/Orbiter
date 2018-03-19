1fd835c136f0f9cfe6256d52032b3b39c065fb0d
aestaria <farah@stanford.edu>
2018-03-18 20:22:49 -0700

    slight adjustments

--- Assets/Scripts/LevelBoundary.cs
+++ Assets/Scripts/LevelBoundary.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelBoundary : MonoBehaviour {
-	public Color loadToColor = Color.white;
 
 	private void OnTriggerEnter2D(Collider2D other) {
 		if (other.CompareTag ("Player")) {
 			string scene = SceneManager.GetActiveScene().name;
-			Initiate.Fade(scene, loadToColor, 0.8f);	
-//			SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+			Initiate.Fade(scene, Color.black, 0.8f);	
 		}
 	}
 }
