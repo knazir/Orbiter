ee11fd44fa8ca0a847fefe9d6e0c2b14d30d5676
Tariq Patanam <t.ahmed.p@gmail.com>
2018-03-21 02:17:56 -0700

    Working magic comet to LEAVE scene

---
+++ Assets/Scripts/Physics/CometExit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CometExit : MonoBehaviour {
+
+	public Vector2 exitTarget;
+
+	private const float FLY_SPEED = 20.0f;
+	private const float EXIT_TARGET_DIST = 1000.0f;
+
+	private bool shouldFlyAway = false;
+
+	// Use this for initialization
+	void Start () {
+		exitTarget = new Vector2 (transform.position.x + EXIT_TARGET_DIST, transform.position.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (shouldFlyAway) {
+			transform.position = Vector2.MoveTowards (transform.position, exitTarget, FLY_SPEED * Time.deltaTime);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.gameObject.tag == Constants.PLAYER) {
+			shouldFlyAway = true;
+		}
+	} 
+}
