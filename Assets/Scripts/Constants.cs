3d492f9e78f3ca56b85da67ea0999c8784fc9fbb
Tariq Patanam <t.ahmed.p@gmail.com>
2018-03-20 23:46:51 -0700

    Working black hole

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 public static class Constants {
 	public const string MOVING_BODY = "MovingCelestialBody";
 	public const string CELESTIAL_BODY = "CelestialBody";
+	public const string SUN = "Sun";
 	public const string PLAYER = "Player";
 	public const string PLANET_TRIGGER = "PlanetTrigger";
 	public const string BOOSTER = "Booster";
