a4a840aa21d5e599774997f53ac5ac154372f09c
Tariq Patanam <t.ahmed.p@gmail.com>
2018-03-20 17:35:42 -0700

    Working boosts with boost bar

--- Assets/Scripts/StatsCounter.cs
+++ Assets/Scripts/StatsCounter.cs
@@ -4,29 +4,30 @@ using UnityEngine;
 
 public class StatsCounter : MonoBehaviour {
 
+	public SimpleHealthBar boostBar;
+
+	private const int MAX_BOOSTS = 10;
 	[SerializeField] private int defaultBoost = 1;
 	private int extraBoosts = 0; // Extra boosts are collected
 
-//	// Use this for initialization
-//	void Start () {
-//		
-//	}
-//	
-//	// Update is called once per frame
-//	void Update () {
-//		
-//	}
+	void Awake() {
+		boostBar = GameObject.Find ("/UI/Boost Bar/Boost Bar").GetComponent<SimpleHealthBar> ();
+		updateBoostBar ();
+	}
 
 	void OnTriggerEnter2D(Collider2D col) {
 		if (col.gameObject.tag == Constants.BOOSTER) {
-			extraBoosts++;
 			Destroy (col.gameObject);
+
+			extraBoosts++;
+			updateBoostBar ();
 		}
 	}
 
 	//////////// Helper Methods /////////////
 	public void replenishDefaultBoost() {
 		defaultBoost = 1;
+		updateBoostBar ();
 	}
 
 	public void useBoost() {
@@ -37,9 +38,14 @@ public class StatsCounter : MonoBehaviour {
 			defaultBoost--;
 		else
 			extraBoosts--;
+		updateBoostBar ();
 	}
 		
 	public bool canUseBoost() {
 		return defaultBoost > 0 || extraBoosts > 0;
+	}
+
+	private void updateBoostBar(){
+		boostBar.UpdateBar (defaultBoost + extraBoosts, MAX_BOOSTS);
 	}
 }
